4de12aca7c6ad93b4ca4c6b18275f66c2808c04f
gzwawj <936588357@qq.com>
2018-09-04 17:25:48 +0800

    bat文件操作完成

--- init.bat
+++ init.bat
@@ -1,20 +1,22 @@
 @echo off
-
-:: 创建配置文件
-del /f /q vimrc\_vimrc
-echo filetype off >>vimrc\_vimrc
-echo set nocompatible >>vimrc\_vimrc
-echo set rtp+=%cd%\vimfiles\bundle\Vundle.vim >>vimrc\_vimrc
-echo call vundle#begin('%cd%\vimfiles\bundle\')>>vimrc\_vimrc
-:: 引入插件文件
-type plugin\plugin.txt>>vimrc\_vimrc
-
-:: 拷贝文件
-copy vimrc\_vimrc ..\
+::设置路径变量
+set dirPath=%cd%
 
 :: 切换目录和创建目录
 cd ..\
 md vimfiles\bundle
+
+:: 创建配置文件
+del /f /q %dirPath%\vimrc\_vimrc
+echo filetype off >>%dirPath%\vimrc\_vimrc
+echo set nocompatible >>%dirPath%\vimrc\_vimrc
+echo set rtp+=%cd%\vimfiles\bundle\Vundle.vim >>%dirPath%\vimrc\_vimrc
+echo call vundle#begin('%cd%\vimfiles\bundle\')>>%dirPath%\vimrc\_vimrc
+:: 引入插件文件
+type %dirPath%\plugin\plugin.txt>>%dirPath%\vimrc\_vimrc
+
+:: 拷贝文件
+copy %dirPath%\vimrc\_vimrc %cd%
 
 :: git获取插件管理
 git clone --depth 1 https://github.com/VundleVim/Vundle.vim.git vimfiles/bundle/Vundle.vim
